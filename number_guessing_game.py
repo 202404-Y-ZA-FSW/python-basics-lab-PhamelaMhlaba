76b2068aadde68c3c17026ca53ca807b83274b01
Phamela Mhlaba <Phamelamhlaba@gmail.com>
2024-08-19 12:48:09 +0200

    Solve assignment

---
+++ number_guessing_game.py
@@ -0,0 +1,16 @@
+import random
+
+target_number = random.randint(1, 100)
+attempts = 0
+
+while True:
+    guess = int(input("Guess a number between 1 and 100: "))
+    attempts += 1
+
+    if guess < target_number:
+        print("Too low! Try again.")
+    elif guess > target_number:
+        print("Too high! Try again.")
+    else:
+        print(f"Congratulations! You guessed the correct number in {attempts} attempts.")
+        break
